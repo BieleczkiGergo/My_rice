436e3af3281866d0848533fd5566433ac23395de
Bieleczki Gergő <bgergo8b@gmail.com>
2025-01-27 13:28:35 +0100

    made fastfetch install interactive

--- install_fastfetch_config.sh
+++ install_fastfetch_config.sh
@@ -2,8 +2,24 @@
 
 ./export_logos.sh
 
-install logos/avocado_hollow.txt ~/.config/fastfetch/logo_l.txt -m 444
-install logos/ubuntu_small.txt ~/.config/fastfetch/logo_S.txt -m 444
+path_to_l=$1
+path_to_s=$2
+
+if [ "$path_to_l" = "" ];
+then
+    path_to_l="logos/avocado_hollow.txt"
+fi
+
+if [ "$path_to_s" = "" ];
+then
+    path_to_s="logos/ubuntu_small.txt"
+fi
+
+echo "Big logo: $path_to_l"
+echo "Small logo: $path_to_s"
+
+install "$path_to_l" ~/.config/fastfetch/logo_l.txt -m 444
+install "$path_to_s" ~/.config/fastfetch/logo_S.txt -m 444
 
 install fastfetch/big_config.jsonc ~/.config/fastfetch/ -m 444
 install fastfetch/small_config.jsonc ~/.config/fastfetch/ -m 444
