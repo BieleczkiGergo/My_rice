3d37a93099659137e64df77ac86d30fb58dc9d5d
Bieleczki Gergő <bgergo8b@gmail.com>
2025-01-30 07:54:08 +0100

    fixed logo renaming issue

--- export_logos.sh
+++ export_logos.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-./logos/generators/ubuntu.sh >logos/ubuntu.txt
-./logos/generators/ubuntu_small.sh >logos/ubuntu_small.txt
+./logos/generators/ubuntu_l.sh >logos/ubuntu_l.txt
+./logos/generators/ubuntu_s.sh >logos/ubuntu_s.txt
 
-./logos/generators/avocado.sh >logos/avocado.txt
-./logos/generators/avocado_hollow.sh >logos/avocado_hollow.txt
+./logos/generators/avocado_l.sh >logos/avocado_l.txt
+./logos/generators/avocado_hollow_l.sh >logos/avocado_hollow_l.txt
--- install_fastfetch_config.sh
+++ install_fastfetch_config.sh
@@ -7,19 +7,19 @@ path_to_s=$2
 
 if [ "$path_to_l" = "" ];
 then
-    path_to_l="logos/avocado_hollow.txt"
+    path_to_l="logos/ubuntu_l.txt"
 fi
 
 if [ "$path_to_s" = "" ];
 then
-    path_to_s="logos/ubuntu_small.txt"
+    path_to_s="logos/ubuntu_s.txt"
 fi
 
 echo "Big logo: $path_to_l"
 echo "Small logo: $path_to_s"
 
 install "$path_to_l" ~/.config/fastfetch/logo_l.txt -m 444
-install "$path_to_s" ~/.config/fastfetch/logo_S.txt -m 444
+install "$path_to_s" ~/.config/fastfetch/logo_s.txt -m 444
 
 install fastfetch/big_config.jsonc ~/.config/fastfetch/ -m 444
 install fastfetch/small_config.jsonc ~/.config/fastfetch/ -m 444
